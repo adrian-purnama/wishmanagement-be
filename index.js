d980510dbeb687e6a97c0b6e0a03fae690dceb3c
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2025-06-16 19:37:34 +0700

    first commit

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function startServer() {
         // );
         // console.log('TTL index for unverified users created');
 
-        app.listen(3000, () => console.log('Server started on port 3000'));
+        app.listen(3003, () => console.log('Server started on port 3003'));
     } catch (err) {
         console.error('DB Connection Error:', err);
         process.exit(1);
